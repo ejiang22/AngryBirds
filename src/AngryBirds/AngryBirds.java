cfa69fe4527e64e1091ae020c575805745047851
ejiang22 <ejiang22@gmail.com>
2015-03-07 17:27:00 -0500

    First test commit to KatieBranch

--- src/AngryBirds/AngryBirds.java
+++ src/AngryBirds/AngryBirds.java
@@ -12,6 +12,8 @@ public class AngryBirds extends Applet implements KeyListener, MouseListener{
 
 int[] foo = {1,2,3,4,5};
 boolean[] bar = {true,false,true,true};
+double fooBar = {2.3,2.4,2.5,2.7};
+int test=0;
 	public AngryBirds() throws HeadlessException {
 		// TODO Auto-generated constructor stub
 		
--- src/AngryBirds/Bird.java
+++ src/AngryBirds/Bird.java
@@ -1,7 +1,7 @@
 package AngryBirds;
 
 public class Bird {
-
+int xpos=0,ypos=0;
 	public Bird() {
 		// TODO Auto-generated constructor stub
 	}
